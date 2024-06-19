681e9a781ae3bf50ecf847177943ca4df2a9b816
amit65a <neupaneamit65@gmail.com>
2024-06-19 21:59:40 +0530

    suman

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,9 @@ app.use(cors())
 
 const port = 5000
 app.use(express.json())
-
+app.use("/test", (req, res) => {
+    res.json("Hello guys")
+})
 app.use("/api/auth", require('./routes/auth'))
 app.use("/api/notes", require('./routes/notes'))
 app.listen(port, () => {
